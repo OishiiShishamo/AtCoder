36ebe7b93e156f9743f1823f7cdb7d2caf5f5c33
Shishamo <shishamodesu@outlook.jp>
2025-07-09 00:43:15 +0900

    ABC238A

---
+++ ABC238A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+    if (n < 5 && n != 1) {
+        std::cout << "No" << std::endl;
+    }
+    else {
+        std::cout << "Yes" << std::endl;
+    }
+    return 0;
+}
