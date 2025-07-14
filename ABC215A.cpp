fb8b68055a5148eaf01c1aaeca01259b6b46cc6b
Shishamo <shishamodesu@outlook.jp>
2025-07-14 23:13:21 +0900

    ABC215A

---
+++ ABC215A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::string s = "";
+    std::cin >> s;
+    if (s == "Hello,World!") {
+        std::cout << "AC" << std::endl;
+    }
+    else {
+        std::cout << "WA" << std::endl;
+    }
+    return 0;
+}
