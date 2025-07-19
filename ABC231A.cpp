f1e8fb5270cbd6fc3c1aed682846fa586268315a
OishiiShishamo <shishamodesu@outlook.jp>
2025-07-19 14:27:37 +0900

    ABC231A

---
+++ ABC231A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    double d = 0;
+    std::cin >> d;
+    std::cout << d / 100 << std::endl;
+    return 0;
+}
