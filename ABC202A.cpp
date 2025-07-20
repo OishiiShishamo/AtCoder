45502752ea23dfe34c156441e90c91de6722431e
OishiiShishamo <shishamodesu@outlook.jp>
2025-07-21 01:48:11 +0900

    ABC202A

---
+++ ABC202A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    int a = 0, b = 0, c = 0;
+    std::cin >> a >> b >> c;
+    std::cout << 21 - (a + b + c) << std::endl; 
+    return 0;
+}
