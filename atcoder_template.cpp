ac0efec038c05ed1362c61b5c726c446951324de
OishiiShishamo <shishamodesu@outlook.jp>
2025-07-21 02:55:37 +0900

    template create

---
+++ atcoder_template.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+#define ALL(a) (a).begin(), (a).end()
+using ll = long long;
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    return 0;
+}
