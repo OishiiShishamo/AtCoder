cdd6cf2bbc3b50135ae22602ed8cbceb5031b60b
OishiiShishamo <shishamodesu@outlook.jp>
2025-11-05 05:06:30 +0900

    ABC216C

---
+++ ABC216C.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+using vll = std::vector<ll>;
+using vvll = std::vector<vll>;
+using vd = std::vector<double>;
+using vvd = std::vector<vd>;
+
+#define ALL(a) (a).begin(), (a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define RANGE_CIN(a) for (auto& x : a) std::cin >> x;
+#define CHMAX(x,y) x = std::max(x,y)
+#define CHMIN(x,y) x = std::min(x,y)
+#define UNIQUE_ERASE(v) v.erase(std::unique(ALL(v)), v.end());
+#define F_DIG_NUM(a) std::fixed << std::setprecision(a)
+#define REP(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define REPR(i,n) for(ll i = (ll)(n); i >= 0; i--)
+#define FOR(i, a, b) for (ll i = (a); i < (b); i++)
+#define EB(x) emplace_back(x)
+#define EM(x) emplace(x)
+#define PSORT(v, fs) std::sort(ALL(v), [](const auto& a, const auto& b) { return a.fs < b.fs; })
+#define PRSORT(v, fs) std::sort(RALL(v), [](const auto& a, const auto& b) { return a.fs < b.fs; })
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    ll n = 0;
+    std::string result;
+    std::cin >> n;
+    while (n != 0) {
+        if (n % 2 == 0) {
+            result += "B";
+            n /= 2;
+        }
+        else {
+            result += "A";
+            n--;
+        }
+    }
+    std::reverse(ALL(result));
+    std::cout << result << std::endl;
+    return 0;
+}
